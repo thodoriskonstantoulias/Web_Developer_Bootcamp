24c20041e357270b0767a7d97107c6bba259a3db
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-01 00:24:16 +0200

    Intro to Javascript : Functions

--- Javascript_Introduction/app.js
+++ Javascript_Introduction/app.js
@@ -27,3 +27,36 @@
 // var userInput3 = prompt("Type your name");
 // var nameCapitalFirst = userInput3.slice(0,1).toUpperCase() + userInput3.slice(1,).toLowerCase();
 // alert("Your name is " + nameCapitalFirst);
+
+
+//Functions 
+
+//Simple Functions
+function testFunc(){
+    console.log("Inside a function");
+} 
+
+testFunc();
+
+//Functions with arguments
+function testFunc2(name){
+    console.log("Inside a function with argument : " + name);
+} 
+
+testFunc2(3);
+
+//Functions with return values 
+function testFunc3(name){
+    return name;
+} 
+
+console.log(testFunc3("Ted"));
+
+//Exercise 4 : Find the BMI as a function
+function bmiCalculator(weight,height){
+    return weight/Math.pow(height,2);
+}
+
+var weight = prompt("Enter your weight");
+var height = prompt("Enter your height");
+console.log("BMI calculator according your input is : " + Math.round(bmiCalculator(weight,height)));
