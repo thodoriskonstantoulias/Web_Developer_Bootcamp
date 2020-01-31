1559aff7b8eacbda92a900d7da1ab95aee47c15b
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-01-31 21:43:53 +0200

    Intro to Javascript : Alerts, console, Data types, Variables and String manipulation

---
+++ Javascript_Introduction/app.js
@@ -0,0 +1,29 @@
+//alert("Hello");
+
+//Data types 
+// console.log(typeof(123));
+// console.log(typeof("name"));
+// console.log(typeof(true));
+
+//Variables 
+//var name = prompt("What is your name");
+//alert(name);
+
+//Strings
+ 
+//alert("Hello " + "World");
+// var name2 = "Ted";
+// console.log("Number of chars are : " + name2.length);
+
+//Exercise : Inform user how many chars has typed and tell the remaining from 140 
+// var userInput = prompt("Type your tweet");
+// alert("You have typed : " + userInput.length + " charactes and you have " + (140-userInput.length) + " remaining"); 
+
+//Exercise 2 : Slice the charactes up to 10 
+// var userInput2 = prompt("Type your tweet");
+// alert("Your message is " + userInput2.slice(0,10));
+
+//Exercise 3 : Ask user for their name and capitalize only first letter
+// var userInput3 = prompt("Type your name");
+// var nameCapitalFirst = userInput3.slice(0,1).toUpperCase() + userInput3.slice(1,).toLowerCase();
+// alert("Your name is " + nameCapitalFirst);
