39cb98ccf4beb94b441f70d337d8ea5f7d1fa4ed
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-01 17:35:01 +0200

    Intro to Javascript : Arrays and for loops

--- Javascript_Introduction/app2.js
+++ Javascript_Introduction/app2.js
@@ -1,17 +1,38 @@
 //Intermidiate Javascript 
 
 //Random 
-console.log(Math.random());
+// console.log(Math.random());
 
 //If we want numbers 1-6 
-console.log(Math.floor(Math.random()*6) + 1);
+// console.log(Math.floor(Math.random()*6) + 1);
 
 //Exercise 1 : Ask for two names and show a randon number as 0-100%
-var name1 = prompt("Enter a name");
-var name2 = prompt("Enter a name");
-var randomPer = Math.floor(Math.random()*100) + 1;
-alert("Match is : " + randomPer + "%");
+// var name1 = prompt("Enter a name");
+// var name2 = prompt("Enter a name");
+// var randomPer = Math.floor(Math.random()*100) + 1;
+// alert("Match is : " + randomPer + "%");
 
 //Comparators 
 // === checks if two values have the same values and data types 
 // !== , <, >, <=, >=
+
+//Arrays 
+// var numbers = [1,2,3,4,5];
+// console.log(numbers);
+// console.log(numbers.includes(2));
+
+//Exercise 2 : FizzBuzz Game
+for (var i=0; i<100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0){
+        console.log("Fizz");
+    }
+    else if (i % 5 === 0){
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
