47de9b5359fbfe371425f9a02495f8f033c9b099
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-01 23:56:22 +0200

    DOM Advanced : Higher Order Functions

--- DOM_Advanced/app.js
+++ DOM_Advanced/app.js
@@ -10,8 +10,30 @@
 //     alert("Clicked");
 // });
 
-for (var i = 0; i < document.querySelectorAll('.drum').length; i++){
-    document.querySelectorAll('.drum')[i].addEventListener('click',function(){
-        alert("Clicked");
-    });
+// for (var i = 0; i < document.querySelectorAll('.drum').length; i++){
+//     document.querySelectorAll('.drum')[i].addEventListener('click',function(){
+//         alert("Clicked");
+//     });
+// }
+
+//Note : Higher Order Functions are functions that can take other functions as inputs
+
+//Exercise 1 : Call dynamically all calcualtions using HOF
+function add(x,y) {
+    return x + y;
 }
+function subtract(x,y) {
+    return x - y;
+}
+function multiply(x,y) {
+    return x * y;
+}
+function divide(x,y) {
+    return x / y;
+}
+
+function calculate(x,y,operator){
+    return operator(x,y);
+}
+
+console.log(calculate(2,3,divide));
