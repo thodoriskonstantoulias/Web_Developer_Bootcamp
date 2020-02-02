0e6744f2f929243fe3b1383491d9046bdd1cdef3
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-03 00:25:21 +0200

    Node Introduction : Installing eternal packages (also check comments as notes)

--- Node_Introduction/app.js
+++ Node_Introduction/app.js
@@ -1,5 +1,22 @@
 //Node
-const fs = require("fs");
+// const fs = require("fs");
 
 //Test a file function
-fs.copyFileSync("file1.txt","file2.txt");
+// fs.copyFileSync("file1.txt","file2.txt");
+
+//General note : Run npm init which created package.json, after we installed an external package for testing which added to package.json as a dependency
+
+
+//Download a module and test --> Superheroes module
+
+var superheroes = require("superheroes");
+var mySuperHero = superheroes.random();
+
+console.log(mySuperHero);
+
+//Download a module and test --> Supervillains module
+
+var supervillains = require("supervillains");
+var mySuperVillain = supervillains.random();
+
+console.log(mySuperVillain);
