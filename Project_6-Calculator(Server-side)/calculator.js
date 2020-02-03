4a0d0bb7b29b0fc1cec902fc0b16100428707401
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-03 21:58:19 +0200

    #Project 6 : Express.js: Calculator

---
+++ Project_6-Calculator(Server-side)/calculator.js
@@ -0,0 +1,25 @@
+//Make a calculator with Express.js
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+
+//Below if we navigate to the home url we will send a response with the associated html file
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html")
+});
+
+//We have to handle the POST request too when the form is submitted 
+app.post("/", function(req,res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    
+    res.send("The result of the calculation is : " + result);
+});
+
+//Our server must listen to a port
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+}); 
