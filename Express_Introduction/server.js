6c63e06cc687cb41f07c576e483d671905f0c995
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-03 21:04:01 +0200

    Express Introduction : Routing and using nodemon

--- Express_Introduction/server.js
+++ Express_Introduction/server.js
@@ -3,12 +3,27 @@
 const express = require("express");
 const app = express();
 
-//Below if we navigate to the root url we will send a response
+//Below if we navigate to the home url we will send a response
 app.get("/", function(request,response){
     response.send("Hello");
+});
+
+//Below if we navigate to the contact url we will send a response
+app.get("/contact", function(req,res){
+    res.send("Contact Page");
+});
+
+//Below if we navigate to the about url we will send a response
+app.get("/about", function(req,res){
+    res.send("About Page");
+});
+
+//Below if we navigate to the hobbies url we will send a response
+app.get("/hobbies", function(req,res){
+    res.send("Hobbies Page");
 });
 
 //Our server must listen to a port
 app.listen(3000, function(){
     console.log("Server started on port 3000");
-});
+}); 
