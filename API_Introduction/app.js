8f2c0c5a231dd2c43a6aa5161eb40bce992f8084
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-04 21:50:01 +0200

    API Introduction : Parsing JSON data and displaying them , res.write functionality

--- API_Introduction/app.js
+++ API_Introduction/app.js
@@ -10,10 +10,22 @@ app.get("/", function(req,res){
 });
 
 //We must make a request to an external server to get the API for crypto values 
+//Also to tsansform json data to objects we use JSON.parse method and the reverese is JSON.stringify
 app.post("/", function(req,res){
-     request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error,response,body){
-         console.log(body);
-     });
+     var crypto = req.body.crypto;
+     var fiat = req.body.fiat;
+     var finalString = crypto + fiat;
+     request(`https://apiv2.bitcoinaverage.com/indices/global/ticker/${finalString}`, function(error,response,body){
+         var data = JSON.parse(body);
+         var price = data.last;
+         var currentDate = data.display_timestamp;
+
+         //If we want to send many commands back we use res.write and then send
+         res.write(`<p>The current date is ${currentDate}</p>`);
+         res.write(`<h1>The price of ${crypto} is ${price} ${fiat}</h1>`);
+         //console.log(price);
+         res.send();
+     });  
 });
 
 app.listen(3000, function(){
