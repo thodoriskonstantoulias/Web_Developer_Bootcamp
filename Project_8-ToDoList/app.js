6353ffa0a644b3df5cbd3deebc28a22aec4a7927
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-05 23:45:27 +0200

    #Project 8 To do list  Part 2 : Creating our first template with EJS

--- Project_8-ToDoList/app.js
+++ Project_8-ToDoList/app.js
@@ -2,20 +2,47 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+//We will use EJS to embed code to html 
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extended : true}));
 
 //Instead of just sending some text we could write some logic 
 //res.write() to send multiple statements 
 //or sendFile() to send an html page with whatever we want
+//or send a template such as EJS here with res.render();
 app.get("/", function(req,res){
     var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
 
-    if (today.getDay() === 6 || today.getDay() === 0){
-        res.send("It is weekend");
-    } else {
-        //res.send("It is a working day");
-        res.sendFile(__dirname + "/index.html");
+    switch(currentDay){
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break; 
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break; 
+        default:
+            console.log("Error");      
     }
+    res.render("list", {kindOfDay : day});
 });
 
 app.listen(3000, function(){
