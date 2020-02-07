baf48a9c896e852cf49496bb040531a20edefadd
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-07 22:27:16 +0200

    #Project 9 Blog Part 5 : Save posts to array, presented them with EJS to home page

--- Project_9-Blog/app.js
+++ Project_9-Blog/app.js
@@ -8,6 +8,9 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+//Global variables
+const posts = [];
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-    res.render("home", {startingContent : homeStartingContent});
+    res.render("home", {startingContent : homeStartingContent, posts : posts});
 });
 
 app.get("/about", function(req,res){
@@ -32,8 +35,15 @@ app.get("/compose", function(req,res){
 });
 
 app.post("/compose", function(req,res){
-  let post = req.body.postTitle;
-  console.log(post);
+  const title = req.body.postTitle;
+  const body = req.body.postBody;
+  const post = {
+    title : title,
+    content : body
+  };
+  posts.push(post);
+
+  res.redirect("/");
 });
 
 
