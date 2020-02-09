aad82ed251c316c8f1fb4ca07ca82f5b5641b898
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-09 12:03:19 +0200

    #Project 8 To do list --Updated Version--  Part 4 : Deletes checked task

--- Project_8-ToDoList/app.js
+++ Project_8-ToDoList/app.js
@@ -88,6 +88,18 @@ app.post("/", function(req,res){
     // }   
 });
 
+app.post("/delete", function(req,res){
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId,function(err){
+        if(err){
+            console.log(err);
+        } else {
+            console.log("Successfully deleted");
+            res.redirect("/");
+        }
+    });
+});
+
 app.get("/work", function(req,res){
     res.render("list", {listTitle : "Work Items", itemsToAdd : workItems});
 }); 
