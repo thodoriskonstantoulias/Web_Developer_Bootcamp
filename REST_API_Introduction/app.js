ab3f81979bcee24bfa9dd276bf69cf0ac9b59b35
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-09 21:16:30 +0200

    REST API introduction : DELETE API

--- REST_API_Introduction/app.js
+++ REST_API_Introduction/app.js
@@ -53,6 +53,17 @@ app.post("/articles", function(req, res){
     });
 });
 
+//DELETE API
+app.delete("/articles", function(req,res){
+  Article.deleteMany(function(err){
+    if (!err){
+      res.send("Successfully deleted all articles!"); 
+    } else {
+      res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function() {
 console.log("Server started on port 3000");
 });
