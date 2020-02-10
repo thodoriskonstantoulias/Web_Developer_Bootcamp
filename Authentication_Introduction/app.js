fe734e53e39fdd31a14326cd6e01362367d4ccb0
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-10 21:43:52 +0200

    Authentication Introduction : Register and Login with MongoDB -- Bad approach

--- Authentication_Introduction/app.js
+++ Authentication_Introduction/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,6 +10,17 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+    email: String,
+    password : String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+//Approach 1 : Saving passwprd as text -- Bad approach
 
 app.get("/", function(req,res){
     res.render("home");
@@ -18,8 +30,39 @@ app.get("/login", function(req,res){
     res.render("login");
 });
 
+app.post("/login", function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if (err){
+            console.log(err);
+        } else {
+            if (foundUser){
+                if (foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
+
 app.get("/register", function(req,res){
     res.render("register");
+});
+
+app.post("/register", function(req,res){
+    const newUser = new User({
+        email : req.body.username,
+        password : req.body.password
+    });
+    newUser.save(function(err){
+        if (err){
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
 });
 
 app.listen(3000, function() {
