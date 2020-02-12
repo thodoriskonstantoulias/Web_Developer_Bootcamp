1320c95eb8330ef58e4ed03f991fe23ad470bdc0
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-02-12 22:59:07 +0200

    Authentication Introduction : Finishing the project

--- Authentication_Introduction/app.js
+++ Authentication_Introduction/app.js
@@ -40,7 +40,8 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({ 
     email: String,
-    password : String
+    password : String,
+    secret: String
 });
 
 //Add passport to our schema
@@ -127,11 +128,22 @@ app.post("/login", function(req,res){
 });
 
 app.get("/secrets", function(req,res){
-    if (req.isAuthenticated()){
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
-    }
+
+    User.find({"secret" : {$ne:null}}, function(err, foundUsers){
+        if (err){
+            console.log(err);
+        } else {
+            if (foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+
+    // if (req.isAuthenticated()){
+    //     res.render("secrets");
+    // } else {
+    //     res.redirect("/login");
+    // }
 });
 
 app.get("/register", function(req,res){
@@ -179,6 +191,33 @@ app.post("/register", function(req,res){
     //     }
     // });
 });
+
+
+app.get("/submit", function(req,res){
+    if (req.isAuthenticated()){
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.post("/submit", function(req,res){
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user._id, function(err, foundUser){
+        if (err){
+            console.log(err);
+        } else {
+            if (foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
+
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
